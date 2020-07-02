1f67c94a3c49bf155aba5b9c06499719a9de0b6a
joro2404 <joro2404@abv.bg>
2020-07-02 13:19:21 +0300

    all models except user added

--- app/category.py
+++ app/category.py
@@ -2,8 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Category(db.Model):
+    __tablename__ = "categories"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), unique=False)
+    name = db.Column(db.String(128), unique=False, nullable=False)
 
-    def __init__(self, anem):
+    def __init__(self, name):
         self.name = name
--- app/product.py
+++ app/product.py
@@ -2,14 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Product(db.Model):
+    __tablename__ = "products"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), unique=False)
-    price = db.Column(db.Float(5, 2), unique=False)
-    overallRaiting = db.Column(db.Float(1, 2), unique=False)
-    #category = db.Column(db.Integer, ForeignKey('category.id'))
+    name = db.Column(db.String(256), unique=False, nullable=False)
+    price = db.Column(db.Float(5, 2), unique=False, nullable=False)
+    overallRaiting = db.Column(db.Float(1, 2), unique=False, nullable=False)
+    #category = db.Column(db.Integer, ForeignKey('category.id'), nullable=False)
 
     def __init__(self, name, price, overallRaiting):
         self.name = name
         self.price = price
         self.overallRaiting = overallRaiting
+        #self.category = category
 
