3f938ee2e520a403f31d342270559b3f77bae185
Veselin Angelov <veskbg@gmail.com>
2020-07-02 16:04:31 +0300

    fixed auth

--- app/__init__.py
+++ app/__init__.py
@@ -4,21 +4,23 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import os
 
+
 db = SQLAlchemy()
+
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = secret_key
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql+psycopg2://postgres:password@localhost:5432/store'
-    #db = SQLAlchemy(app)
     db.init_app(app)
 
     login_manger = LoginManager()
     login_manger.login_view = 'auth.login'
     login_manger.init_app(app)
 
-    from .user import User
+    from .models import User
 
     @login_manger.user_loader
     def load_user(id):
--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, Blueprint
 from flask_login import current_user, login_required
-from .product import Product
-from .user import User
+from .models import User
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html', name=current_user.name)
+    return render_template('index.html')
+
+
+@main.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
 
 
 if __name__ == '__main__':
